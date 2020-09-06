a6998af3e4bd129f4ec1880ec64e556607576c62
Eliezer Silva <djosacv@gmail.com>
2020-09-07 01:40:30 +0200

    Update remote-notebook-forward.sh

--- remote-jupyter-notebook/remote-notebook-forward.sh
+++ remote-jupyter-notebook/remote-notebook-forward.sh
@@ -6,5 +6,6 @@ then
 else
 	echo "creating notebook server and forwarind port"
 	ssh $2 './notebook_serve.sh '"$1"''
+	echo "finishing port-forwarding copy the above link in the browser"
 	ssh -N -L localhost:$1:localhost:$1 $2
 fi
