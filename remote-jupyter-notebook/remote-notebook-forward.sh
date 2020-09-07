7ff603d90751e245a9218923a02bc0252cfec1da
Eliezer Silva <djosacv@gmail.com>
2020-09-07 09:56:41 +0200

    Update remote-notebook-forward.sh

--- remote-jupyter-notebook/remote-notebook-forward.sh
+++ remote-jupyter-notebook/remote-notebook-forward.sh
@@ -4,14 +4,14 @@ then
 	echo "killing remote notebook server"
 	ssh $2 './notebook_serve.sh '"$1"' kill'
 else
-	echo "creating notebook server and forwardind port"
+	echo "creating notebook server and port-forwarding"
 	if [ "$3" = "conda" ] 
 	then
 		ssh $2 './notebook_serve.sh '"$1"' '"$3"' '"$4"
 	else
 		ssh $2 './notebook_serve.sh '"$1"''
 	fi
-	echo "finishing port-forwarding copy the above link in the browser"
+	echo "finishing port-forwarding. copy the above link in the browser"
 	ssh -N -L localhost:$1:localhost:$1 $2
 fi
 
