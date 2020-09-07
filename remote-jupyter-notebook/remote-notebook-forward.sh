bd9d2364a74ab2ea806b1492c976b66e41c25722
Eliezer Silva <djosacv@gmail.com>
2020-09-07 03:40:39 +0200

    change remote conda environment to run the notebook

--- remote-jupyter-notebook/remote-notebook-forward.sh
+++ remote-jupyter-notebook/remote-notebook-forward.sh
@@ -4,8 +4,15 @@ then
 	echo "killing remote notebook server"
 	ssh $2 './notebook_serve.sh '"$1"' kill'
 else
-	echo "creating notebook server and forwarind port"
-	ssh $2 './notebook_serve.sh '"$1"''
+	echo "creating notebook server and forwardind port"
+	if [ "$3" = "conda" ] 
+	then
+		ssh $2 './notebook_serve.sh '"$1"' '"$3"' '"$4"
+	else
+		ssh $2 './notebook_serve.sh '"$1"''
+	fi
 	echo "finishing port-forwarding copy the above link in the browser"
 	ssh -N -L localhost:$1:localhost:$1 $2
 fi
+
+
